008515dd224cac58eb62e6676e793ba956bf23d7
Henrique Massareli <henrique@oneseventech.com>
2023-05-05 00:01:37 -0300

    feat: add ships migration

---
+++ database/migrations/1683244727756_ships.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'ships'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('fuel_capacity').notNullable()
+      table.integer('fuel_level').notNullable()
+      table.integer('weight_capacity').notNullable()
+
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
