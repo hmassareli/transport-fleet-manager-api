a3887599a7623db8e410362ba763a2e44b0e8ed1
Henrique Massareli <henrique@oneseventech.com>
2023-05-05 21:37:50 -0300

    feat: add validation for payload request

--- app/Controllers/Http/ContractsController.ts
+++ app/Controllers/Http/ContractsController.ts
@@ -1,15 +1,37 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Contract from 'App/Models/Contract'
 
+export enum Planets {
+  Andvari = 'Andvari',
+  Demeter = 'Demeter',
+  Aqua = 'Aqua',
+  Calas = 'Calas',
+}
+export enum Resources {
+  minerals = 'minerals',
+  water = 'water',
+  food = 'food',
+}
 export default class ContractsController {
   public async index({}: HttpContextContract) {
-    return await Contract.all()
+    return await Contract.findBy('status', 'idle')
   }
 
   public async create({}: HttpContextContract) {}
 
   public async store(ctx: HttpContextContract) {
     const { payload: resourcesData, ...contractData } = ctx.request.body()
+    if (Planets[contractData.originPlanet] === undefined) {
+      throw new Error('Invalid origin planet')
+    }
+    if (Planets[contractData.destinationPlanet] === undefined) {
+      throw new Error('Invalid destination planet')
+    }
+    resourcesData.map((resource) => {
+      if (Resources[resource.name.toLowerCase()] === undefined) {
+        throw new Error('Invalid resource')
+      }
+    })
     const contract = await Contract.create(contractData)
     await contract.related('payload').createMany(resourcesData)
     await contract.load('payload')
