ff565018513412db3add4dbfd0438d0249876e70
Henrique Massareli <henrique@oneseventech.com>
2023-05-05 21:34:49 -0300

    feat: add foreign key on contracts table

--- database/migrations/1683244988888_contracts.ts
+++ database/migrations/1683244988888_contracts.ts
@@ -3,13 +3,22 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'contracts'
 
+  //Andvari	Demeter	Aqua	Calas are the possible planets
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.enum('status', ['idle', 'accepted', 'in_progress', 'completed']).defaultTo('idle')
       table.string('description')
-      table.string('origin_planet')
+      table.enum('origin_planet', ['Andvari', 'Demeter', 'Aqua', 'Calas'])
       table.string('destination_planet')
       table.integer('value')
+      table
+        .integer('pilot_id')
+        .unsigned()
+        .references('id')
+        .inTable('pilots')
+        .onDelete('CASCADE')
+        .nullable()
 
       table.timestamps(true, true)
     })
